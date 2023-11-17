08dd8ee29d2f72de5cc726681baf0379d045bffd
egasa21 <esaputra168@gmail.com>
2023-11-18 01:47:57 +0700

    import class R

--- app/src/main/java/com/lazzy/stories/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/lazzy/stories/ui/maps/MapsActivity.kt
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.lazzy.stories.R
 import com.lazzy.stories.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
